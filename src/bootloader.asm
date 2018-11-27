f04c14022dac2e75e1b39c888676fdf1a17fe38e
Dmitry Khokhlov <d.khokhlov@targetix.net>
2018-11-27 09:41:20 +0300

    bootloader: add STACK_SIZE constant

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,10 +1,13 @@
         BITS 16
 
+STACK_SIZE \
+        equ 4096
+
 start:
         mov ax, 07C0h           ; Set up 4K stack space after this bootloader
         add ax, 288             ; (4096 + 512) / 16 bytes per paragraph
         mov ss, ax
-        mov sp, 4096
+        mov sp, STACK_SIZE
 
         mov ax, 07C0h           ; Set data segment to where we're loaded
         mov ds, ax
